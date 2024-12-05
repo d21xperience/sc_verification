c1914c51a49139215e312608a3ea55b2166ce79f
deden mj <d21xperience@gmail.com>
2024-12-05 17:52:28 +0700

    tes baru

--- frontend/src/views/sc_ijazah/SendTrx.vue
+++ frontend/src/views/sc_ijazah/SendTrx.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
         <h1>Hello from send TRX</h1>
+        <div class="border mx-auto p-3 w-1/2 rounded-sm">
+            <div>
+                <label for="sender">Pengirim</label>
+                <input type="text" name="sender" id="sender" class="border p-2 w-full"
+                    placeholder="Masukan alamat pengirim">
+
+            </div>
+            <div>
+                <label for="receiver">Penerima</label>
+                <input type="text" name="receiver" id="receiver" class="border p-2 w-full"
+                    placeholder="Masukan alamat penerima">
+            </div>
+
+            <div>
+                <button>Kirm</button>
+            </div>
+        </div>
+
     </div>
 </template>
 
